3d3a3e5bb995a50f7f8b0ce05fd69c25527b018e
ANIRUDDH KUMAR <96242079+aniruddhy@users.noreply.github.com>
2022-02-24 21:41:57 +0530

    asdsd

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,8 @@ const Welcome = () => {
   const name = "Aniruddh";
   return (
     <>
-      <h1>Hey !</h1>
-      <h2>Welcome {name} to Newton School.</h2>
+      <h1>Hey {name}</h1>
+      <h2>Welcome to Newton School.</h2>
     </>
   );
 };
