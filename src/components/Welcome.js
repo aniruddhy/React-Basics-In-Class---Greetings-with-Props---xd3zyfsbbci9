1c9e9f425d86af02e12eac87cf8444e1efb372e6
ANIRUDDH KUMAR <96242079+aniruddhy@users.noreply.github.com>
2022-02-24 21:39:28 +0530

    sdfdf

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Welcome = () => {
+  const name = "Aniruddh";
   return (
     <>
       <h1>Hey !</h1>
-      <h2>Welcome to Newton School.</h2>
+      <h2>Welcome {name} to Newton School.</h2>
     </>
   );
 };
